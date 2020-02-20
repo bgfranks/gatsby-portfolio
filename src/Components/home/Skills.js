ebfbbdc5fe505c1e5bf124992871c98ac5c0befe
Brandon Franks <bgfranks106@icloud.com>
2020-02-20 17:26:15 -0500

    created a skills component and added a brief description of my skills

---
+++ src/Components/home/Skills.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+const Skills = () => {
+  return (
+    <div>
+      <h2>My Skills</h2>
+      <div>
+        <div>
+          <h3>Front End</h3>
+          <p>
+            Proficiency with HTML, CSS, and JavaScript. Specailizing with React,
+            Styled Components, and Sass. Also have some experience with Less,
+            GatsbyJS, NextJS, and some unit testing with React.
+          </p>
+        </div>
+        <div>
+          <h3>Back End</h3>
+          <p>
+            Some experience with NodeJS and Express. Also familiar with Graphql
+            using the Graphql Playground interface and some knowledge of Graphql
+            Yoga. Familiar with SQL Databases and NoSQL databases with MongoDB.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h3>My Focus</h3>
+        <p>
+          I specialize in front end development but I do have knowledge of back
+          end technologies as well. I enjoy adding subtle animations to my
+          projects, and creating clean modern layouts.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../Components/global/Layout"
 import Head from "../Components/global/Head"
 import CTA from "../Components/home/CTA"
+import Skills from "../Components/home/Skills"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
       <CTA />
+      <Skills />
     </Layout>
   )
 }
